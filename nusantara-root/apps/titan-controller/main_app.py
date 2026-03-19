a1eee1216c5bd8115cadd8da1a95a5367d62b406
Sovereign Titan Genesis <admin@quorumstate.international>
2026-03-19 06:05:00 -0400

    Create main_app.py

---
+++ nusantara-root/apps/titan-controller/main_app.py
@@ -0,0 +1,5 @@
+from packages.quorum_protocol.consensus import QuorumProtocol
+
+def monitor_mesh():
+    # Logika untuk mengawasi dilatasi waktu dari tiap node
+    print("Sovereign Titan Monitoring: Active")
