c7f5ea177d03009d31b7f3e3dab1a36fc0826891
Sovereign Titan Genesis <admin@quorumstate.international>
2026-03-24 13:45:04 -0400

    Update QSTATE.sol

--- contracts/QSTATE.sol
+++ contracts/QSTATE.sol
@@ -5,26 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title QuorumStateCoin
- * @dev Versi Sempurna dari kode lama Anda.
- * Menambahkan fitur Ownable untuk keamanan dan batas 1.000T.
+ * @title QuorumStateToken
+ * @dev Suplai Sovereign Scale: 100.000 Triliun (100 Quadrillion)
+ * Mencerminkan nilai sejarah Nusantara yang tak terukur.
  */
-contract QuorumStateCoin is ERC20, Ownable {
-    // Batas Maksimal Suplai Global: 1.000 Triliun (1.000T)
-    uint256 public constant MAX_SUPPLY = 1000000000000000 * 10**18;
+contract QuorumStateToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 100000000000000000 * 10**18;
 
-    constructor() ERC20("Quorum State", "QSTATE") Ownable(msg.sender) {
-        // Fase Genesis: Cetak 1 Triliun pertama langsung ke dompet Anda
-        _mint(msg.sender, 1000000000000 * 10**18);
+    constructor() ERC20("Quorum State Token", "QSTATE") Ownable(msg.sender) {
+        _mint(msg.sender, MAX_SUPPLY);
     }
 
-    /**
-     * @dev Fungsi khusus untuk mencetak koin fase berikutnya (H2K Mining).
-     * Hanya bisa dipanggil oleh Anda (Owner).
-     */
-    function mintNextPhase(address to, uint256 amountInTrillions) public onlyOwner {
-        uint256 amountToMint = amountInTrillions * 1000000000000 * 10**18;
-        require(totalSupply() + amountToMint <= MAX_SUPPLY, "Melebihi batas maksimal 1000T!");
-        _mint(to, amountToMint);
+    function emergencyMint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Kedaulatan maksimal tercapai.");
+        _mint(to, amount);
     }
 }
