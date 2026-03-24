25ae1adc9d9a69080d10a8d1e75c7a4855588acb
Sovereign Titan Genesis <admin@quorumstate.international>
2026-03-24 11:52:03 -0400

    Update firmware.ino

--- HARDWARE/firmware.ino
+++ HARDWARE/firmware.ino
@@ -1,1 +1,18 @@
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+void setup() {
+  Serial.begin(115200);
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) while (1);
+  particleSensor.setup();
+}
+void loop() {
+  long irValue = particleSensor.getIR();
+  if (checkForBeat(irValue) == true) {
+    Serial.print("QS_MINTED_KEY:STG-");
+    Serial.println(millis());
+  }
+}
 
