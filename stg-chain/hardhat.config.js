28c5ff536c60311306f6b2e35d18de0d77a1884b
Andi Muhammad Harpianto <you@example.com>
2026-05-13 23:43:20 +0000

    GENESIS: Initialized STG-Chain Block 0 with Sovereign Metadata

---
+++ stg-chain/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+
+    bscTestnet: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
