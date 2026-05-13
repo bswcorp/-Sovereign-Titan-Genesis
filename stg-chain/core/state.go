378519a8a79bc3e107e629b5a02d58a106c7f963
Andi Muhammad Harpianto <you@example.com>
2026-05-13 21:24:51 +0000

    INFRA: Configured transaction registries and state balance tracking loops inside engine core

--- stg-chain/core/state.go
+++ stg-chain/core/state.go
@@ -1,30 +1,85 @@
 package core
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"sync"
+	"time"
 )
 
+type Transaction struct {
+	Hash  string `json:"hash"`
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value uint64 `json:"value"`
+}
+
 type StateDB struct {
-	mu          sync.RWMutex
+	mu           sync.RWMutex
 	CurrentBlock uint64
+	Balances     map[string]uint64
+	Transactions []Transaction
 }
 
 func NewStateDB() *StateDB {
-	return &StateDB{
+	s := &StateDB{
 		CurrentBlock: 1,
+		Balances:     make(map[string]uint64),
+		Transactions: []Transaction{},
 	}
+
+	// Genesis Wallet
+	s.Balances["0xgenesis"] = 1000000
+	return s
 }
 
 func (s *StateDB) IncrementBlock() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-
 	s.CurrentBlock++
 }
 
 func (s *StateDB) GetBlock() uint64 {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-
 	return s.CurrentBlock
+}
+
+func (s *StateDB) GetBalance(addr string) uint64 {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.Balances[addr]
+}
+
+func (s *StateDB) SendTransaction(from string, to string, value uint64) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.Balances[from] < value {
+		return "", fmt.Errorf("insufficient funds")
+	}
+
+	s.Balances[from] -= value
+	s.Balances[to] += value
+
+	raw := fmt.Sprintf("%s:%s:%d:%d", from, to, value, time.Now().UnixNano())
+	hash := sha256.Sum256([]byte(raw))
+	txHash := "0x" + hex.EncodeToString(hash[:])
+
+	tx := Transaction{
+		Hash:  txHash,
+		From:  from,
+		To:    to,
+		Value: value,
+	}
+
+	s.Transactions = append(s.Transactions, tx)
+	return txHash, nil
+}
+
+func (s *StateDB) GetTransactions() []Transaction {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.Transactions
 }
--- stg-chain/rpc/server.go
+++ stg-chain/rpc/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
 	"stg-chain/core"
 )
 
@@ -49,10 +51,43 @@ func rpcHandler(w http.ResponseWriter, r *http.Request) {
 	switch req.Method {
 	case "eth_chainId":
 		resp.Result = "0x309"
+
 	case "web3_clientVersion":
-		resp.Result = "STG-Chain/v0.2"
+		resp.Result = "STG-Chain/v0.3"
+
 	case "eth_blockNumber":
 		resp.Result = fmt.Sprintf("0x%x", state.GetBlock())
+
+	case "eth_getBalance":
+		if len(req.Params) < 1 {
+			resp.Error = "missing address param"
+			break
+		}
+		addr := req.Params[0].(string)
+		balance := state.GetBalance(addr)
+		resp.Result = fmt.Sprintf("0x%x", balance)
+
+	case "eth_sendTransaction":
+		if len(req.Params) < 1 {
+			resp.Error = "missing transaction parameters"
+			break
+		}
+		tx := req.Params[0].(map[string]interface{})
+		from := tx["from"].(string)
+		to := tx["to"].(string)
+		valueStr := tx["value"].(string)
+
+		value, _ := strconv.ParseUint(valueStr, 10, 64)
+		hash, err := state.SendTransaction(from, to, value)
+		if err != nil {
+			resp.Error = err.Error()
+		} else {
+			resp.Result = hash
+		}
+
+	case "stg_transactions":
+		resp.Result = state.GetTransactions()
+
 	default:
 		resp.Error = "method not supported"
 	}
